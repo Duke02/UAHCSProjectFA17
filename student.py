3e6dae385721239ff2d03681c60f3e8096ecdbeb
Trystan May <tkmshere@yahoo.com>
2017-10-23 22:37:05 -0500

    Revamped student class to include some features talked in tonight's meeting.
    Also created a new class for actual classes for college. With professors and what not.

--- student.py
+++ student.py
@@ -11,19 +11,39 @@ class Student:
     def __init__(self):
         self.classes = []
         self.gpa = 0.0
-        self.morale = 50
+        self.stress = 50
+        self.knowledge = 50
+        self.dexterity = 50
+        self.strength = 50
+        self.speed = 50
+        self.courses = []
 
-    """
-    Class attendance increases upon going to class
-    """
+    def liftWeights(self, weightOfWeights):
+        self.strength += weightOfWeights/20
 
-    def goToClass(self, whichClass):
-        whichClass.attendance += 1
+    def runOnTreadmill(self, speedOfRun):
+        if speedOfRun >= 3.5:
+            self.speed += speedOfRun
 
-    """
-    Or be responsible and finish a task and feel accomplished, boosting morale
-    """
+    def addToStress(self, amt):
+        if self.stress + amt >= 100:
+            self.stress = 100
+        elif self.stress + amt <= 0:
+            # ie amt is negative
+            self.stress = 0
+        else:
+            self.stress += amt
 
-    def adult(self, task):
-        task.completed = True
-        self.morale += 10
+    def setStress(self, stress):
+        if 0 <= stress <= 100:
+            self.stress = stress
+
+    def getGPA(self):
+        sum = 0
+        for clazz in self.classes:
+            sum += clazz.grade
+        return sum / len(self.classes)
+
+    def registerForClass(self, clazz):
+        self.classes += clazz
+
